4b5de1258907f60f3029fc0b851242a82f9efd3e
Chaudoreille <a.chaudoreille@gmail.com>
2023-02-08 12:57:07 +0100

    add content for gallery + styling

--- app.js
+++ app.js
@@ -37,7 +37,19 @@ app.get('/gallery', (req, res) => {
     const galleryImages = [
         {
             src: "https://cdn.britannica.com/06/202006-050-64C85CC7/Neil-deGrasse-Tyson-2018.jpg",
-            alt: "Neil deGrass Tyson in a suit",
+            alt: "Neil deGrass Tyson in a suit, looking good",
+        },
+        {
+            src:"https://3.bp.blogspot.com/-jeTseRMwjJY/UyCn2KxGXwI/AAAAAAAAbxU/mKDj8HJXol0/s1600/neil-degrasse-tyson.jpg",
+            alt: "Neil deGrass Tyson body language",
+        },
+        {
+            src:"https://www.thewrap.com/wp-content/uploads/2015/08/neil-degrasse-tyson.jpg",
+            alt: "Neil deGrass Tyson on national geographics engaged in explanation",
+        },
+        {
+            src:"https://startalkmedia.com/wp-content/uploads/2019/08/More-Science-in-Pop-Fiction_Neil-deGrasse-Tyson-Charles-Liu-and-Chuck-Nice_Credit_StarTalk_1400x975.jpg",
+            alt: "Neil deGrass Tyson on StarTalk",
         },
     ]
     res.render("Gallery", {title: "Gallery", gallery: galleryImages})
